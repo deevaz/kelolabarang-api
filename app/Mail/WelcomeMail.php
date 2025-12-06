9de4120be1271abd6c91b2f86f1950a6fc273869
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2025-12-06 10:00:55 +0700

    feat: mailtrap integration

---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Attachment;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Headers;
+use Illuminate\Queue\SerializesModels;
+use Mailtrap\EmailHeader\CategoryHeader;
+use Mailtrap\EmailHeader\CustomVariableHeader;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Header\UnstructuredHeader;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private string $name;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address('jeffrey@abdaziz.my.id', 'Regita Cahya'),
+            replyTo: [
+                      new Address('taylor@abdaziz.my.id', 'Abdul Aziz'),
+                  ],
+            subject: 'Welcome Mail',
+            using: [
+                      function (Email $email) {
+                          // Headers
+                          $email->getHeaders()
+                              ->addTextHeader('X-Message-Source', 'abdaziz.my.id')
+                              ->add(new UnstructuredHeader('X-Mailer', 'Mailtrap PHP Client'))
+                          ;
+
+                          // Custom Variables
+                          $email->getHeaders()
+                              ->add(new CustomVariableHeader('user_id', '45982'))
+                              ->add(new CustomVariableHeader('batch_id', 'PSJ-12'))
+                          ;
+
+                          // Category (should be only one)
+                          $email->getHeaders()
+                              ->add(new CategoryHeader('Integration Test'))
+                          ;
+                      },
+                  ]
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.welcome-email',
+            with: ['name' => $this->name],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [
+            Attachment::fromPath('https://mailtrap.io/wp-content/uploads/2021/04/mailtrap-new-logo.svg')
+                ->as('logo.svg')
+                ->withMime('image/svg+xml'),
+        ];
+    }
+
+    /**
+     * Get the message headers.
+     */
+    public function headers(): Headers
+    {
+        return new Headers(
+            'custom-message-id@abdaziz.my.id',
+            ['previous-message@abdaziz.my.id'],
+            [
+                'X-Custom-Header' => 'Custom Value',
+            ],
+        );
+    }
+}
